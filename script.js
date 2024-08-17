0451cf355ac0428e28d0bddbf5abd80fc3daaf80
Pablomenegassolp <pablomenegasso@gmail.com>
2024-08-17 14:18:55 +0000

    aula11

---
+++ script.js
@@ -0,0 +1,16 @@
+function livro(nome, ano, autor) {
+
+}
+
+
+livro("Elon Musk", 2023, "Walter Isaacson");
+const nomeDoLivro = "Elon Musk";
+console.log(nomeDoLivro);
+const autorDoLivro = "Walter Isaacson";
+console.log(autorDoLivro);
+const frase = `${nomeDoLivro} por ${autorDoLivro}`;
+console.log(frase);
+const anoDeLancamento = 2023;
+const anoDesdeLancamento = 2024 - anoDeLancamento;
+console.log(anoDesdeLancamento);
+
