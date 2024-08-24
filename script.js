9b22bd467de52ab024fd47c41fd48849b81b83d3
Pablomenegassolp <pablomenegasso@gmail.com>
2024-08-24 13:52:03 +0000

    aula 12

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
+/*
 function livro(nome, ano, autor) {
 
 }
-
 
 livro("Elon Musk", 2023, "Walter Isaacson");
 const nomeDoLivro = "Elon Musk";
@@ -14,3 +14,33 @@ const anoDeLancamento = 2023;
 const anoDesdeLancamento = 2024 - anoDeLancamento;
 console.log(anoDesdeLancamento);
 
+*/
+/*
+onmousemove = function(e){
+    document.querySelector('.circulo').style.top = (e.clientY-8)+"px";
+    document.querySelector('.circulo').style.left = (e.clientX-8)+"px";
+}
+    */
+   /*
+const circulo = document.querySelector('.circulo');
+function moverCirculo(event) {
+    const larguraCirculo = circulo.offsetWidth
+    const alturaCirculo = circulo.offsetHeight
+    const valorTop = event.clientY - (alturaCirculo/2)
+    const valorLeft = event.clientX - (larguraCirculo/2)
+     circulo.style.top = valorTop + 'px'
+     circulo.style.top = valorLeft + 'px'
+}
+*/
+
+const btn = document.querySelector('button');
+
+function cliqueBotao() {
+  console.log('Clicou');
+}
+function somar {
+    const
+
+}
+
+btn.addEventListener('click', cliqueBotao);
